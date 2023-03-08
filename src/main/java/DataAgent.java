76d3f496af574a433ddc2d305a194a2c24e3ec89
Caio Godoy <caio.godoy.vg@gmail.com>
2023-03-08 09:12:31 -0300

    adding updateCurrentCapacity method in DataAgent

--- src/main/java/DataAgent.java
+++ src/main/java/DataAgent.java
@@ -42,23 +42,42 @@ public class DataAgent {
         }
     }
 
-    public void addTrash(int trashCode, String type, int maxCapacity, int currentCapacity, Building building) {
+    public void addTrash(int trashCode, String type, float maxCapacity, float currentCapacity, int buildingCode) {
 
         try {
             // Prepare a statement to insert a new building
-            String command = "INSERT INTO trash (trashCode, type, maxCapacity, currentCapacity, buildingCode) VALUES (?, ?, ?, ?)";
+            String command = "INSERT INTO trash (trashCode, type, maxCapacity, currentCapacity, buildingCode) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(command);
             statement.setInt(1, trashCode);
             statement.setString(2, type);
-            statement.setInt(3, maxCapacity);
-            statement.setInt(4, currentCapacity);
-            statement.setInt(5, building.getBuildingCode());
+            statement.setFloat(3, maxCapacity);
+            statement.setFloat(4, currentCapacity);
+            statement.setInt(5, buildingCode);
 
             // Execute the statement and close it
             statement.executeUpdate();
             statement.close();
 
             log.info(".addTrash: done");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateCurrentCapacity(float increaseOfTheDay, int buildingCode) {
+
+        try {
+            // Prepare a statement to insert a new building
+            String command = "UPDATE trash SET currentCapacity = currentCapacity + ? WHERE buildingCode = ?";
+            PreparedStatement statement = connection.prepareStatement(command);
+            statement.setFloat(1, increaseOfTheDay);
+            statement.setInt(2, buildingCode);
+
+            // Execute the statement and close it
+            statement.executeUpdate();
+            statement.close();
+
+            log.info(".addCurrentCapacity: done");
         } catch (SQLException e) {
             e.printStackTrace();
         }
